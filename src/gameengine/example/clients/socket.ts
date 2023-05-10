edd6631a9ae1035c9a1b17167d4abfcc518dc04a
alexeySe <uberfull@mail.ru>
2023-05-10 13:23:21 +0300

    tast socket-io client

--- src/gameengine/example/clients/socket.ts
+++ src/gameengine/example/clients/socket.ts
@@ -1,69 +1,75 @@
 import { ConsoleClient } from './console'
-import * as net from 'net'
+import { io } from 'socket.io-client'
 import { Method, SocketMessage } from '../socket-server-game'
 
 const HOST = 'localhost'
-const PORT = 11051
+const PORT = 7000
 
 const consoleClient = new ConsoleClient()
 
-const socket = new net.Socket()
+const socket = io(`http://${HOST}:${PORT}`)
 
-socket.on('error', (err) => console.error(err))
-socket.on('close', () => console.error('closed'))
-socket.on('connect', () => console.log('connected'))
-socket.on('data', (data) => {
-  socketDataHandler(data).catch(console.error)
+
+socket.on('connect', () => {
+  socket.emit('start')
+  console.log('connected')
+  })
+
+socket.on('error', (err) => {
+  console.error(err)
 })
 
-socket.connect({
-  host: HOST,
-  port: PORT
+socket.on('disconnect', () => {
+  console.error('disconnected')
 })
 
-let buffer: string = ''
+socket.on('data', async (data) => {
+  await socketDataHandler(data)
+  console.log(data)
+})
 
-async function socketDataHandler (chunk: Buffer): Promise<void> {
-  console.log(`Received chunk: ${chunk.toString()}`)
-  const data: string = buffer + chunk.toString()
+
+
+
+
+async function socketDataHandler (data:any): Promise<void> {
+  console.log(`Received chunk: ${data}`)
+ 
   const messages = data.split('\r')
-  if (messages.length === 1) {
-    buffer = data
-  } else {
-    buffer = messages.pop() ?? ''
+  data = messages.pop() ?? ''
     for (const messageStr of messages) {
       const message: SocketMessage = JSON.parse(messageStr)
       await messageHandler(message)
+      console.log(await messageHandler(message))
     }
   }
-}
 
-async function messageHandler (message: SocketMessage): Promise<void> {
-  let answer
-  switch (message.method) {
-    case Method.GIVE_CARDS:
-      await consoleClient.giveCards(message.cards ?? [])
-      await socket.write(JSON.stringify({ method: Method.GIVE_CARDS }) + '\r')
-      break
-    case Method.SET_MONEY:
-      await consoleClient.setCoins(message.count ?? 0)
-      await socket.write(JSON.stringify({ method: Method.SET_MONEY }) + '\r')
-      break
-    case Method.RETURN_SPY:
-      answer = await consoleClient.returnSpy()
-      await socket.write(JSON.stringify({ method: Method.RETURN_SPY, action: answer }) + '\r')
-      break
-    case Method.SEND_SPY:
-      answer = await consoleClient.placeSpy()
-      await socket.write(JSON.stringify({ method: Method.SEND_SPY, action: answer }) + '\r')
-      break
-    case Method.TAKE_OFF_CARDS:
-      answer = await consoleClient.takeOffCards(message.count ?? 0)
-      await socket.write(JSON.stringify({ method: Method.TAKE_OFF_CARDS, cardIds: answer }) + '\r')
-      break
-    case Method.TURN:
-      answer = await consoleClient.turn()
-      await socket.write(JSON.stringify({ method: Method.TURN, turn: answer }) + '\r')
-      break
+  async function messageHandler (message: SocketMessage): Promise<void> {
+    let answer
+    switch (message.method) {
+      case Method.GIVE_CARDS:
+        await consoleClient.giveCards(message.cards ?? [])
+        await socket.emit('data', (JSON.stringify({ method: Method.GIVE_CARDS }) + '\r'))
+        break
+      case Method.SET_MONEY:
+        await consoleClient.setCoins(message.count ?? 0)
+        await socket.emit('data', (JSON.stringify({ method: Method.SET_MONEY }) + '\r'))
+        break
+      case Method.RETURN_SPY:
+        answer = await consoleClient.returnSpy()
+        await socket.emit('data', (JSON.stringify({ method: Method.RETURN_SPY, action: answer }) + '\r'))
+        break
+      case Method.SEND_SPY:
+        answer = await consoleClient.placeSpy()
+        await socket.emit('data', (JSON.stringify({ method: Method.SEND_SPY, action: answer }) + '\r'))
+        break
+      case Method.TAKE_OFF_CARDS:
+        answer = await consoleClient.takeOffCards(message.count ?? 0)
+        await socket.emit('data', (JSON.stringify({ method: Method.TAKE_OFF_CARDS, cardIds: answer }) + '\r'))
+        break
+      case Method.TURN:
+        answer = await consoleClient.turn()
+        await socket.emit('data', (JSON.stringify({ method: Method.TURN, turn: answer }) + '\r'))
+        break
+    }
   }
-}
